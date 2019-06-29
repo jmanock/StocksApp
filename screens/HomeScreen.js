9f8e2e410b139207c412a5df4776647c6abe1825
Jon Manock <jonmanock@Jons-MacBook-Pro.local>
2019-06-29 10:31:09 -0400

    testing

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -3,90 +3,31 @@ import {TouchableOpacity,StyleSheet,TextInput,Text,View} from 'react-native';
 import StocksAPI from '../api/StocksAPI';
 
 export default class HomeScreen extends Component{
+  constructor(props){
+    super(props);
+  }
   state = {
-    date:new Date(),
-    symbole:'',
-    name:'',
-    yesterdayPrice:'',
-    todayPrice:'',
-    timeRefreshed:'',
-    close:'',
+    symbol:'',
     data:[]
   }
-  componentWillMount(){
-    /*
-      ~ Need todays date, yesterdays date √
-      ~ Maybe get news?
-      ~ Show stock
-        - Name
-        - Price
-        - Change
-        - Update Time
-      ~ Flat list?
-      ~ Time till close or Open
-      ~ Maybe look for companies & symboles?
-    */
-
-  }
-  getDate = () =>{
-    var year = this.state.date.getFullYear();
-    var month = this.state.date.getMonth() + 1;
-    var day = this.state.date.getDate();
-    var yesterday = this.state.date.getDate() -1;
-    if(month < 10){
-      month = '0'+month;
-    }
-    if(day < 10){
-      day = '0'+day;
-    }
-    StocksAPI.getDate(year,month,day,yesterday);
-  }
-
-  getStockQuote = () =>{
-    StocksAPI.getStocks(this.state.symbole).then(function(data){
+  search = () =>{
+    StocksAPI.getAllCompanies(this.state.symbol).then(function(data){
       this.setState({
-        time:data.time,
-        todayPrice:data.closePrice,
-        yesterdayPrice:data.yesterdayClose,
-        name:data.name,
-        change:data.change,
-        symbole:''
-      });
-    }.bind(this))
-  }
-  // something = () =>{
-  //   StocksAPI.getCompanies(this.state.symbole).then(function(data){
-  //
-  //     // this.setState({
-  //     //   symbolLook:data.symbol,
-  //     //   nameLook:data.name,
-  //     //   symbole:''
-  //     // });
-  //   }.bind(this));
-  something = () =>{
-    StocksAPI.getCompanies(this.state.symbole).then(function(data){
-      this.setState({
-        data:data,
-        symbole:''
+        data:data
       });
     }.bind(this));
   }
   render(){
     return(
       <View style={styles.container}>
-        <TextInput placeholder='Enter Symbol' value={this.state.symbole} onChangeText={(text) =>this.setState({symbole:text})} style={styles.txt} placeholderTextColor={'white'}/>
+        <TextInput placeholder='Enter Comapany Name or Symbol' value={this.state.symbol} onChangeText={(text) =>this.setState({symbol:text})} style={styles.txt} placeholderTextColor={'white'}/>
 
-        <TouchableOpacity onPress={this.something}>
-          <Text style={styles.btnText}>Get Quote</Text>
+        <TouchableOpacity onPress={this.search}>
+          <Text style={styles.btnText}>Search</Text>
         </TouchableOpacity>
 
-        <Text>{this.state.name}</Text>
-        <Text>{this.state.todayPrice}</Text>
-        <Text>{this.state.change}</Text>
-        <Text>{this.state.symbolLook}</Text>
-        <Text>{this.state.nameLook}</Text>
+        <Text>{this.state.data.name}</Text>
       </View>
-
     )
   }
 }
